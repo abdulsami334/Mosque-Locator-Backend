4ebe94c4bca7bd3bd8ed0516da74778336228a4c
unknown <codewithsami05@gmail.com>
2026-06-12 04:56:09 -0700

    Fix: removed env and node_modules from github

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// MUST come before any routes
+// Connect Database Pehle karein
+connectDB();
+
+// Middlewares
 app.use(cors());
-app.use(express.json());   // <= parses JSON bodies
+app.use(express.json());   
 
 // Routes
 app.use('/api/contributors', require('./routes/contributorRoutes'));
@@ -15,7 +18,9 @@ app.use('/api/mosques',      require('./routes/mosqueRoutes'));
 app.use('/api/admin',        require('./routes/contributorAdmin'));
 app.use('/api/notifications', require('./routes/notification'));
 
-
-connectDB();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));   
+
+// Host '0.0.0.0' lazmi shamil karein
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
